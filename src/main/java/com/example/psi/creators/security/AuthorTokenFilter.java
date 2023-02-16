92fc3b628265c4e7b2463289a25435cce1a8dce0
Makafui-aheto <59707287+Makafui-aheto@users.noreply.github.com>
2023-02-16 12:32:38 +0000

    Authenticator changes

--- src/main/java/com/example/psi/creators/security/AuthorTokenFilter.java
+++ src/main/java/com/example/psi/creators/security/AuthorTokenFilter.java
@@ -15,9 +15,13 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.IOException;
 
-public class AuthorTokenFilter extends OncePerRequestFilter {
+public class AuthorTokenFilter extends OncePerRequestFilter implements Filter {
 
     @Autowired
     private AuthorJwtUtils jwtUtils;
@@ -60,5 +64,36 @@ public class AuthorTokenFilter extends OncePerRequestFilter {
         }
 
         return null;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws javax.servlet.ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         javax.servlet.FilterChain filterChain) throws IOException, javax.servlet.ServletException {
+        try {
+            String jwt = parseJwt((HttpServletRequest) servletRequest);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+                UserDetails userDetails = authorDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest) servletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e.toString());
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
--- src/main/java/com/example/psi/creators/security/WebSecurityConfig.java
+++ src/main/java/com/example/psi/creators/security/WebSecurityConfig.java
@@ -5,20 +5,23 @@ import com.example.psi.creators.service.AuthorDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -60,47 +63,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new JavaMailSenderImpl();
     }
 
-    @Override
-    public void configure(WebSecurity web){
-        web.ignoring().antMatchers("/swagger-ui/**", "/v2/api-docs",
-                "/configuration/ui",
-                "/v3/api-docs/**",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html/**",
-                "/webjars/**");
-    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/premium/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/premium/**").hasAuthority("create")
-                .antMatchers("/api/v1/permission/add-role").hasAnyAuthority("edit", "create")
-                .antMatchers("/api/v1/group/setup").hasAuthority("create")
-                .antMatchers(HttpMethod.GET,"/api/v1/membership/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/membership/**").hasAuthority("create")
-                .antMatchers(HttpMethod.PUT, "/api/v1/membership/**").hasAuthority("edit")
-                .antMatchers(HttpMethod.POST, "/api/v1/group/**").hasAuthority("create")
-                .antMatchers(HttpMethod.GET, "/api/v1/group/**").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/api/v1/group/**").hasAuthority("delete")
-                .antMatchers(HttpMethod.PUT, "/api/v1/group/**").hasAuthority("edit")
-                .antMatchers("/api/v1/mortality/**").hasAnyAuthority("create", "edit")
-                .antMatchers("/api/v1/company/**").hasAnyAuthority("create", "edit", "read")
-                .antMatchers("/api/v1/payroll/calculator").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/claims/**").hasAuthority("read")
-                .antMatchers(HttpMethod.POST, "/api/v1/claims/**").hasAnyAuthority("create", "edit")
-                .antMatchers(HttpMethod.GET, "/api/v1/staff/**").hasAuthority("read")
-                .antMatchers(HttpMethod.POST, "/api/v1/staff/**").hasAnyAuthority("edit", "create")
-                .antMatchers("/api/v1/server/**").hasAnyAuthority("edit","create")
                 .anyRequest().authenticated();
 
-//         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/swagger-ui/**", "/v2/api-docs",
+                "/configuration/ui",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html/**",
+                "/webjars/**");
 
     }
+
 }
